77d78032e028807a1590af994bc425c4e6831468
Матвей <matveyshakur@gmail.com>
2026-03-15 15:30:44 +0500

    FEATURE: хранение выбранной темы в localStorage

--- frontend/src/providers/ThemeProvider.tsx
+++ frontend/src/providers/ThemeProvider.tsx
@@ -3,10 +3,15 @@ import {createContext, ReactNode, useState} from 'react';
 export const ThemeContext = createContext({theme: 'light', toggleTheme: () => {}});
 
 export const ThemeProvider = ({children}: {children: ReactNode}) => {
-	const [theme, setTheme] = useState('light');
+	const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
 
 	const toggleTheme = () => {
-		setTheme((prevTheme: string) => prevTheme === 'light' ? 'dark' : 'light');
+		setTheme((prevTheme: string) => {
+			const newTheme = prevTheme === 'light' ? 'dark' : 'light';
+
+			localStorage.setItem('theme', newTheme);
+			return newTheme;
+		});
 	};
 
 	return (
